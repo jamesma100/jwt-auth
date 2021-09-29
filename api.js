7810d85b5baedc72701f0b0f03132046d8e7fb26
James Ma <jamesma100@gmail.com>
2021-09-28 23:32:30 -0500

    add api, get asset route not working

---
+++ api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressjwt = require("express-jwt");
+
+const app = express();
+//const PORT = process.env.API_PORT || 8888;
+const PORT = 5555;
+const jwtCheck = expressjwt({
+	secret: "mykey",
+	algorithms: ['HS256']
+});
+
+app.use(bodyParser.json());
+
+app.get("*", (req, res) => {
+	res.status(404).send("Route not defined");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
+
+app.get("/asset", (req, res) => {
+	res.status(200).send("Everybody can see this");
+});
+
+app.get("/asset/secret", jwtCheck, (req, res) => {
+	res.status(200).send("For authorized users");
+});
--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
+const jwt = require("jsonwebtoken");
+
 app.use(bodyParser.json());
+app.use(cors());
 const PORT = 8888;
+
 app.get("/time", (req, res) => {
 	const time = (new Date()).toLocaleTimeString();
 	res.status(200).send(`The time is ${time}`);
 });
+
 app.get("*", (req, res) => {
 	res.sendStatus(404);
 });
+
 app.listen(PORT, () => {
 	console.log(`Server in running on port ${PORT}.`);
 });
+
 app.post("/login", (req, res) => {
-	const user = req.body.username;
-	res.status(200).send(`User's name is ${user}`);
+	if (!req.body.username || !req.body.password) {
+		res.status(400).send("Error. Need username and password");
+		return;
+	}
+	const user = users.find((u) => {
+		return u.username === req.body.username && u.password === req.body.password;
+	});
+	if (!user) {
+		res.status(404).send("User not found or password incorrect.");
+		return;
+	}
+	const token = jwt.sign({
+		sub: user.id,
+		username: user.username
+	}, "mykey", {expiresIn: "3 hours"});
+	res.status(200).send({access_token: token});
 });
+
+// issue JWTs
+const users = [
+	{id: 1, username: "hughjackman", password: "claws"},
+	{id: 2, username: "spiderman", password: "web"}
+];
 
 
